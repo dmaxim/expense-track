bb42209f5a39b327f445e90030d1f624b04fd204
Dan Maxim <maxdanus@yahoo.com>
2019-12-30 14:51:50 -0500

    initial project setup

---
+++ src/Barney.Domain/Models/Expense.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+namespace Barney.Domain.Models
+{
+    public class Expense
+    {
+        private Expense() { }
+
+
+        public Expense(int expenseId, byte classificationId, string description, short ownerId,
+            DateTimeOffset incurredDate)
+        {
+            ExpenseId = expenseId;
+            ExpenseClassificationId = classificationId;
+            Description = description;
+            ExpenseOwnerId = ownerId;
+            IncurredDate = incurredDate;
+        }
+        public int ExpenseId { get; }
+
+        public byte ExpenseClassificationId { get; }
+
+        public string Description { get; }
+
+        public short ExpenseOwnerId { get; }
+
+        public DateTimeOffset IncurredDate { get;  }
+    }
+}
---
+++ src/Barney.Domain/Models/Income.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System;
+
+namespace Barney.Domain.Models
+{
+    public class Income
+    {
+        private Income() { }
+
+        public Income(int incomeId, short classificationId, short employerId, decimal beforeDeductions,
+            decimal afterDeductions,DateTimeOffset transactionDate, short expenseOwnerId)
+        {
+            IncomeId = incomeId;
+            IncomeClassificationId = classificationId;
+            EmployerId = employerId;
+            BeforeDeductions = beforeDeductions;
+            AfterDeductions = afterDeductions;
+            TransactionDate = transactionDate;
+            ExpenseOwnerId = expenseOwnerId;
+        }
+
+        public int IncomeId { get; }
+
+        public short IncomeClassificationId { get; }
+
+        public short EmployerId { get;  }
+
+        public decimal BeforeDeductions { get; }
+
+        public decimal AfterDeductions { get; }
+
+        public DateTimeOffset TransactionDate { get; }
+
+        public short ExpenseOwnerId { get; }
+    }
+}
