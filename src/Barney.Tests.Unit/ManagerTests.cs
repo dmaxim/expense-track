d619564d0c6e674c4e6b86f5d95c248ac79bec78
Dan Maxim <dmaxim@dm-mac.local>
2020-07-07 08:45:43 -0600

    add unit test projecdt

---
+++ src/Barney.Tests.Unit/ManagerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Barney.Tests.Unit
+{
+    public class ManagerTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
